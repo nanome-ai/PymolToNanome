e3056a426ccd003bb083ea432c5b9156dfcd03b4
Xavier M <xavier@nanome.ai>
2024-11-04 17:48:28 +0100

    Update PymolSendToNanome2.py

--- PymolSendToNanome2.py
+++ PymolSendToNanome2.py
@@ -3,6 +3,9 @@
 
 import os
 from pymol.Qt import QtCore
+from pymol import cmd
+import json
+import shutil
 
 loading_gif_url = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"
 nanome_logo_url = "https://pbs.twimg.com/profile_images/988544354162651137/HQ7nVOtg_400x400.jpg"
@@ -42,7 +45,6 @@ def run_plugin_gui():
 
 
 def make_login_dialog():
-    from pymol import cmd
     from pymol.Qt import QtWidgets, QtGui
 
     names = cmd.get_object_list()
@@ -95,7 +97,6 @@ def make_dialog():
     import tempfile
 
     import requests
-    from pymol import cmd
     from pymol.Qt import QtGui, QtWidgets
     global nanome_logo_path
 
@@ -178,6 +179,141 @@ class Worker(QtCore.QObject):
         workspace_api.send_file(self.session_path)
         self.finished.emit()
 
+class PymolToMolz():
+    
+    def int2reps(self, rep):
+        reps = []
+        if rep == 0:
+            return reps
+        if rep & 1:
+            reps.append("ball-and-stick")
+        if rep & 2:
+            reps.append("sphere")
+        if rep & 4:
+            reps.append("surface")
+        if rep & 8:
+            reps.append("label")
+        if rep & 16:
+            reps.append("sphere")
+        if rep & 32:
+            reps.append("cartoon")
+        if rep & 128:
+            reps.append("line")
+        return reps
+
+
+    def color_to_rgb(self, id):
+        from math import floor
+        c = cmd.get_color_tuple(id)
+        return [floor(color * 255) for color in c] + [255]
+
+
+    def prepare_molz_directories(self):
+        import tempfile
+        main_dir = tempfile.TemporaryDirectory(prefix="workspaceJson_").name
+        assets_dir = os.path.join(main_dir, "assets")
+        os.makedirs(assets_dir)
+        return main_dir, assets_dir
+
+
+    def save_structures(self, assets_dir):
+        import tempfile
+        structures = []
+        name_map = {}
+        for mol_name in cmd.get_names('objects'):
+            output_cif = tempfile.NamedTemporaryFile(
+                prefix=mol_name.replace(' ', '_')+"_",
+                suffix=".cif", delete=False, dir=assets_dir).name
+            cmd.save(output_cif, mol_name)
+            basename = os.path.basename(output_cif)
+            name_map[mol_name] = basename
+            structures.append({"Name": mol_name, "Extension": "cif",
+                            "Identifier": name_map[mol_name]})
+        return structures, name_map
+
+
+    def get_representations_per_structure(self, mol_name, name_map):
+        components = []
+        representations = []
+        colors = []
+        cmd.iterate("model " + mol_name + " and enabled",
+                    "representations.append(reps); colors.append(color)")
+
+        rep_types = set(representations)
+        rep_types_s = set([j for i in rep_types for j in self.int2reps(i)])
+
+        data_per_rep = {}
+        for r in rep_types_s:
+            data_per_rep[r] = []
+
+        for i, r in enumerate(representations):
+            for rs in self.int2reps(r):
+                data_per_rep[rs].append(i)
+
+        for i in data_per_rep:
+            cols = [colors[c] for c in data_per_rep[i]]
+            color_set = list(set(cols))
+
+            rep = {
+                "Kind": i,
+                "ColorScheme": {
+                    "Library": [self.color_to_rgb(c) for c in color_set],
+                    "Colors": [color_set.index(c) for c in cols],
+                },
+                "SizeScheme": {
+                    "Kind": "uniform",
+                    "Scale": 1.0,
+                    "BFactorFactor": 0.0
+                },
+                "Parameters": {}
+            }
+            component = {
+                "Structure": name_map[mol_name],
+                "Name": i[0].upper() + i[1:].lower(),
+                "Model": 0,
+                "Selection": data_per_rep[i],
+                "Representations": [rep]
+            }
+            components.append(component)
+        return components
+
+
+    def create_state_file(self, main_dir, structures, components):
+        state = {"Version": "0.0.1",
+                "Structures": structures,
+                "Components": components
+                }
+        with open(os.path.join(main_dir, "state.json"), "w") as f:
+            json.dump(state, f)
+
+
+    def create_molz_archive(self, main_dir):
+        final_path = main_dir + ".molz"
+        shutil.make_archive(main_dir, "zip", main_dir)
+        shutil.rmtree(main_dir)
+        os.rename(main_dir + ".zip", final_path)
+        return final_path
+
+
+    def export_to_molz(self):
+        # Remove atoms that are added by Pymol for missing residues
+        cmd.remove("not present")
+
+        main_dir, assets_dir = self.prepare_molz_directories()
+        structures, name_map = self.save_structures(assets_dir)
+
+        # Get the representation per structure
+        components = []
+
+        for mol_name in cmd.get_names('objects'):
+            components.extend(
+                self.get_representations_per_structure(mol_name, name_map))
+
+        self.create_state_file(main_dir, structures, components)
+        molz_path = self.create_molz_archive(main_dir)
+        print(f"Created molz workspace file: {molz_path}")
+        return molz_path
+
 class WorkspaceAPI():
     def __init__(self, username, passw):
         self.token = None
@@ -204,7 +340,6 @@ class WorkspaceAPI():
     def send_file(self, filepath):
         from datetime import datetime
         import requests
-        import pymol2molz
         
         if self.token is None:
             r_token = self.get_nanome_token()
@@ -214,7 +349,7 @@ class WorkspaceAPI():
             return r_token
         
         try:
-            molz_file = pymol2molz.export_to_molz()
+            molz_file = PymolToMolz().export_to_molz()
         except Exception as e:
             print(f"Error during conversion to .molz file: {e}")
         
